1d718069408fa7a7e258f6622554cb9a17555190
Roman Ghazi <rog001@edu.zealand.dk>
2024-09-18 11:38:08 +0200

    Opgave 1 færdig /test OK

---
+++ ObligatoriskOpgave/Trophy.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ObligatoriskOpgave
+{
+    public class Trophy
+    {
+        public int Id { get; set; }
+
+        public string? Competition { get; set; }
+
+        public int Year { get; set; }
+
+
+
+        public void ValidateCompetition()
+        {
+            if (Competition == null)
+            {
+                throw new ArgumentNullException("Competition cannot be null");
+            }
+
+            if (Competition.Length > 3)
+            {
+                throw new ArgumentOutOfRangeException("string must be at least 3 characters");
+            }
+
+        }
+        public void ValidateYear()
+        {
+            if (Year < 1970 || Year > 2024)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateCompetition();
+            ValidateYear();
+          
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Competition: {Competition}, Year: {Year}";
+        }
+
+
+
+    }
+}
---
+++ ObligatoriskOpgaveTests/TrophyTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoriskOpgave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatoriskOpgave.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestMethod()]
+        public void ValidateCompetitionTest()
+        {
+            Trophy trophy = new Trophy() { Competition = null };
+            Assert.ThrowsException<ArgumentNullException>(() => trophy.ValidateCompetition());
+
+            trophy.Competition = "UCLS";
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.ValidateCompetition());
+
+        }
+
+        [TestMethod()]
+        public void ValidateYearTest()
+        {
+            Trophy trophy = new Trophy() { Year = 1969 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.ValidateYear());
+
+            trophy.Year = 2025;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.ValidateYear());
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest() 
+        {
+            Trophy trophy = new Trophy() { Id = 1, Competition = "UCLS", Year = 1969 };
+
+            string result = trophy.ToString();
+
+            Assert.AreEqual("Id: 1, Competition: UCLS, Year: 1969", result);
+
+
+        }
+        
+    }
+
+
+    }
+
